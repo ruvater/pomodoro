ae38117f96752468136ce5d7c907a1bddcf48435
Vladimir Nazarov <ruvater@gmail.com>
2015-10-27 14:43:25 +0300

    Added seeds

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,6 +2,12 @@ class Setting
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  COLORS = %w(white black red green blue #3D1255)
+  CYCLE_MINUTES = 25
+  SHORT_BREAK_MINUTES = 5
+  LONG_BREAK_MINUTES = 15
+  CYCLES_TO_LONG_BREAK = 4
+
   validates :key, presence: true
 
   field :key, type: String
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+study = Category.create name: 'Study' unless Category.where(name: 'Study').count > 0
+work = Category.create name: 'Work' unless Category.where(name: 'Work').count > 0
+coursera = Category.create name: 'Coursera' unless Category.where(name: 'Coursera').count > 0
+
+
+# Set/update available color setting
+if Setting.where(key: 'colors').count > 0
+  Setting.where(key: 'colors').first.update_attributes(value: Setting::COLORS.join(','))
+else
+  Setting.create key: 'colors', value: Setting::COLORS.join(',')
+end
+
+# Set/update cycle length in minutes setting
+if Setting.where(key: 'cycle_minutes').count > 0
+  Setting.where(key: 'cycle_minutes').first.update_attributes(value: Setting::CYCLE_MINUTES.to_s)
+else
+  Setting.create key: 'cycle_minutes', value: Setting::CYCLE_MINUTES.to_s
+end
+
+# Set/update short break length in minutes setting
+if Setting.where(key: 'short_break_minutes').count > 0
+  Setting.where(key: 'short_break_minutes').first.update_attributes(value: Setting::SHORT_BREAK_MINUTES.to_s)
+else
+  Setting.create key: 'short_break_minutes', value: Setting::SHORT_BREAK_MINUTES.to_s
+end
+
+# Set/update long break length in minutes setting
+if Setting.where(key: 'long_break_minutes').count > 0
+  Setting.where(key: 'long_break_minutes').first.update_attributes(value: Setting::LONG_BREAK_MINUTES.to_s)
+else
+  Setting.create key: 'long_break_minutes', value: Setting::LONG_BREAK_MINUTES.to_s
+end
+
+# Set/update work cycles number to take a long break setting
+if Setting.where(key: 'cycles_to_long_break').count > 0
+  Setting.where(key: 'cycles_to_long_break').first.update_attributes(value: Setting::CYCLES_TO_LONG_BREAK.to_s)
+else
+  Setting.create key: 'cycles_to_long_break', value: Setting::CYCLES_TO_LONG_BREAK.to_s
+end
